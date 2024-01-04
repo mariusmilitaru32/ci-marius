389545baef81a24fb3e870f2e33f5d5f51ecc09f
mariusmilitaru32 <marius.militaru20@gmail>
2024-01-04 11:01:57 +0000

    Adding delete function

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,6 +7,7 @@ from taskmanager.models import Category, Task
 def home():
     return render_template("tasks.html")
 
+
 @app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
@@ -30,5 +31,13 @@ def edit_category(category_id):
         category.category_name = request.form.get("category_name")
         db.session.commit()
         return redirect(url_for("categories"))
-        
+
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
