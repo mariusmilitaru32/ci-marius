b4f7b66f3f07f9e3fd97ade938fc29acc6080652
mariusmilitaru32 <marius.militaru20@gmail>
2024-01-04 12:52:41 +0000

    Add task function and template

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {
+            done: "Select"
+        },
+        defaultDate: new Date(),
+        setDefaultDate: true
+    })
+
+    // select initialization
+    let select = document.querySelectorAll("select");
+    M.FormSelect.init(select);
+
 });
+
+
